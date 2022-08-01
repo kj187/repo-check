8e9e27bf0c651babcce77450b4c3eff0728be87c
Julian Kleinhans <julian.kleinhans@elmer-digital.de>
2022-08-01 14:15:34 +0200

    Update

---
+++ README.md
@@ -0,0 +1,11 @@
+# Repository Checker
+
+```
+repo-check 
+repo-check ./my-sub-directory
+```
+
+
+```bash
+ln -sfnv /Users/julian.kleinhans/__Server/Projects/repo-check/check.sh /usr/local/bin/repo-check
+```
--- check.sh
+++ check.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+_ignore_up_to_date="False"
+
+while getopts p:i flag
+do
+    case "${flag}" in
+        p) _path=${OPTARG:-"./"};;
+        i) _ignore_up_to_date="True";;
+    esac
+done
+
 function start_check() {
+    _ignore_up_to_date=${0}
+    _path=${1}
 
     color_normal="\033[0m"; color_black="\033[30m"; 
     bg_green='\033[42m'; bg_red='\033[41m'; bg_yellow='\033[43m'; bg_blue='\033[44m'; bg_magenta='\033[0;45m'; bg_cyan='\033[0;46m'
@@ -10,30 +22,31 @@ function start_check() {
     function echo_blue { echo -e "${bg_blue}${color_black}$1${color_normal}"; }
     function echo_magenta { echo -e "${bg_magenta}${color_black}$1${color_normal}"; }
 
-    _DIR=${1/.git/}
+    _DIR=${_path/.git/}
     cd $_DIR
 
     if [[ $_DIR == *"__archive"* ]] || [[ $_DIR == *".terraform"* ]] || [[ $_DIR == *"vendor"* ]]; then
         return
     fi
     
-    echo 
-    echo 
-    echo "---------------------------------------------------------------------------------------------"
-    echo "DIR:              ${_DIR}"
-    echo "CURRENT BRANCH:   $(git rev-parse --abbrev-ref HEAD)"
-    echo "CURRENT HASH:     $(git rev-parse HEAD)"
-    echo "REMOTE URL:       $(git config --get remote.origin.url)"
-    echo
-    git remote update
-   
-    echo 
+    git remote update &> /dev/null
 
     UPSTREAM="@{u}"                                     #UPSTREAM=${1:-'@{u}'}
     LOCAL=$(git rev-parse @)
     REMOTE=$(git rev-parse "$UPSTREAM")
     BASE=$(git merge-base @ "$UPSTREAM")
 
+    if [[ $LOCAL = $REMOTE ]] && [[ $_ignore_up_to_date = "True" ]]; then
+        return
+    fi
+
+    echo "---------------------------------------------------------------------------------------------"
+    echo "DIR:              ${_DIR}"
+    echo "CURRENT BRANCH:   $(git rev-parse --abbrev-ref HEAD)"
+    echo "CURRENT HASH:     $(git rev-parse HEAD)"
+    echo "REMOTE URL:       $(git config --get remote.origin.url)"
+    echo
+    
     if [ $LOCAL = $REMOTE ]; then
         echo_green " STATUS:     UP-TO-DATE             "
     elif [ $LOCAL = $BASE ]; then
@@ -42,10 +55,12 @@ function start_check() {
         echo_blue " STATUS:     NEED TO PUSH            "
     else
         echo_red " STATUS:     DIVERGED                 "
-    fi
+    fi 
 
+    echo
+    echo
 
 }
 
 export -f start_check
-find ${1:-.} -name .git -type d -exec bash -c 'start_check "$0"' {} \;
+find ${_path:-.} -name .git -type d -exec bash -c 'start_check "$1" ' $_ignore_up_to_date {} \;
